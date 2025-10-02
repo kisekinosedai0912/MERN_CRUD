883720f179dd87c334e162b52b2c4c3c9808bfdc
jasper <kisekinosedai0912@gmail.com>
2025-10-02 13:05:59 +0800

    full crud operations with error handling middleware integrated

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,30 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { dbConnection } from './config/database.js';
 import Product from './model/Products.js';
+import { AppError } from './utils/errorHandler.js'
+import { errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 const app = express();
 
 app.use(express.json()); // lets us use JSON response from the request body
-app.post('/api/products', async (req, res) => {
+app.get('/api/products', async (req, res, next) => {
+    try {
+        const items = await Product.find({});
+        if (!items) return next(new AppError(404, 'Products not found'));
+        
+        res.status(201).json({success: true, data: items});
+    } 
+    catch (error) {
+        next(error);
+    }
+});
+
+app.post('/api/products', async (req, res, next) => {
     const product = req.body; // user will send the request data to the backend server inside the body property in JSON
 
     if (!product.productName || !product.price || !product.img) {
-        return res.status(400).json({
-            success: false,
-            message: `Please provide a value to all fields required to create a product`
-        });
+        return next(new AppError(400, 'Please provide all required fields'));
     }
 
     const newProduct = new Product(product);
@@ -22,13 +33,41 @@ app.post('/api/products', async (req, res) => {
         await newProduct.save();
         res.status(201).json({success: true, data: newProduct});
 
-    } catch (e) {
-        console.error(`Error creating product: ${e.message}`);
-        res.status(500).json({succes: false, message: `Server error occurred`});
+    } catch (error) {
+        next(error)
     }
 });
 
+app.put('/api/products/:id', async (req, res, next) => {
+    const { id } = req.params;
+    const product = req.body;
+
+    try {
+        const productToUpdate = await Product.findByIdAndUpdate(id, product, {new: true});
+        if (!productToUpdate) return next(new AppError(404, 'No product found with an id of', id));
+
+        res.status(201).json({success: true, message: 'Product updated successfully!', data: productToUpdate})
+    } catch (error) {   
+        next(error);
+    }
+});
+
+app.delete('/api/products/:id', async (req, res, next) => {
+    const { id } = req.params;
+    
+    try {
+        const deletedProduct = await Product.findByIdAndDelete(id);
+        if (!deletedProduct) return next(new AppError(404, 'Product not found'));
+
+        res.status(201).json({sucess: true, message: 'Product deleted successfully!'});
+
+    } catch (error) {   
+        next(error);
+    }
+});
+
+app.use(errorHandler);
 app.listen(5000, () => {
     dbConnection();
     console.log('Server started at https://localhost:5000')
-})
+});
