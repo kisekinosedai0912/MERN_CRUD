d333531f858ab4177459868c171da3cda6913f31
jasper <kisekinosedai0912@gmail.com>
2025-10-06 17:50:56 +0800

    Readyt for production

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors'
+import path from 'path';
 import { dbConnection } from './config/database.js';
 import { errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/products.route.js';
@@ -22,6 +23,17 @@ app.use('/api/products', productRoutes);
 ** Utilizing the global error handler class from './middleware/errorHandler.js'
 */
 app.use(errorHandler);
+/*
+** Listen to other route path aside from what is defined
+*/
+const __dirname = path.resolve();
+
+if (process.env?.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+}
 /*
 ** Listen to port 5000 and connect to database
 */
