59698c8b9afbbff9f809332da9028b3b5a25fe16
jasper <kisekinosedai0912@gmail.com>
2025-10-06 19:47:24 +0800

    temporary URL fix for axios to fetch

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+// I'll only do this for temporary purpose because somehow, my env variable for frontend is not being read by vite
+const API_BASE_URL = 'https://mern-crud-01p8.onrender.com';
 console.log(API_BASE_URL)
 const api = axios.create({
     baseURL: `${API_BASE_URL}/api`,
